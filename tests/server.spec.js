d15a0d83c48b98e61378d4046f4a769350fb418e
mdramirez071 <mdramirez071@gmail.com>
2019-07-15 15:03:03 -0700

    adds test on server spec js file

--- tests/server.spec.js
+++ tests/server.spec.js
@@ -37,4 +37,15 @@ describe('server/app.js', function() {
       done();
     });
   });
+
+  it('page says this is a test string', (done) => {
+    chai.request(server)
+      .get('/')
+      .end((err, res) => {
+        expect(err).not.exist;
+        expect(JSON.stringify(res.text)).to.contain('This is a test string');
+        done();
+      });
+    });
+
 })
